7d4623f898723ae1800f85d8c462f0c6d412e402
Auxurya <cyber@auxurya.com>
2022-01-02 01:43:28 -0500

    Added the counter template & a reset button

--- script.js
+++ script.js
@@ -1,51 +1,63 @@
+let computerScore = 0, playerScore = 0;
+let result = 'PLAY YOUR HAND';
+let results = document.getElementById('results');
+results.innerHTML = `<p>PLAYER [${playerScore}] <span>${result}</span> [${computerScore}] COMPUTER</p>`;
+
+
+let choices = ['Rock', 'Paper', 'Scissors'];
+let computerPlay = () => choices[Math.floor(Math.random() * 3)];
 let playerSelection;
+
 window.addEventListener('click', event => {
     playerSelection = event.target.alt;
     playRound();
 });
 
-let choices = ['Rock', 'Paper', 'Scissors'];
-let computerPlay = () => choices[Math.floor(Math.random() * 3)];
-let computerScore = 0, playerScore = 0;
+
 let playRound = () => {
     let computerSelection = computerPlay();
     switch(playerSelection) {
         case 'Rock':
                 if(computerSelection == 'Paper' || computerSelection == 'Scissors') {
-                    computerSelection == 'Paper' ?
-                             alert(`You lose! ${computerSelection} [${++computerScore}] covers ${playerSelection} [${playerScore}]`) :
-                                     alert(`You win! ${playerSelection} [${++playerScore}] breaks ${computerSelection} [${computerScore}]`);
+
                 } else {
-                    alert(`It's a draw! ${playerSelection} [${playerScore}] matches ${computerSelection} [${computerScore}].`);
+                    results.innerHTML = `<p>PLAYER [${playerScore}] <span>It's a draw! ${playerSelection} matches ${computerSelection}.</span> [${computerScore}] COMPUTER</p>`;
                 };
                 break;
         case 'Paper':
                 if(computerSelection == 'Rock' || computerSelection == 'Scissors') {
-                    computerSelection == 'Rock' ?
-                             alert(`You win! ${playerSelection} [${++playerScore}] covers ${computerSelection} [${computerScore}]`) :
-                                     alert(`You lose! ${computerSelection} [${++computerScore}] cut ${playerSelection} [${playerScore}]`);
+
                 } else {
-                    alert(`It's a draw! ${playerSelection} [${playerScore}] matches ${computerSelection} [${computerScore}].`);
+                    results.innerHTML = `<p>PLAYER [${playerScore}] <span>It's a draw! ${playerSelection} matches ${computerSelection}.</span> [${computerScore}] COMPUTER</p>`;
                 };
                 break;
         case 'Scissors':
                 if(computerSelection == 'Paper' || computerSelection == 'Rock') {
-                    computerSelection == 'Paper' ?
-                             alert(`You win! ${playerSelection} [${++playerScore}] cut ${computerSelection} [${computerScore}]`) :
-                                     alert(`You lose! ${computerSelection} [${++computerScore}] breaks ${playerSelection} [${playerScore}]`);
+
                 } else {
-                    alert(`It's a draw! ${playerSelection} [${playerScore}] matches ${computerSelection} [${computerScore}].`);
+                    results.innerHTML = `<p>PLAYER [${playerScore}] <span>It's a draw! ${playerSelection} matches ${computerSelection}.</span> [${computerScore}] COMPUTER</p>`;
                 };
                 break;
     };
 };
 
-let game = () => {
-    let rounds = prompt('How many rounds of \'Rock, Paper, Scissors\' would you like to play?: ')
-    computerScore = 0, playerScore = 0;
-    for(i = 0; i < rounds; i++) {
-        playRound();
-    };
-    alert(`Final Score: PLAYER [${playerScore}] VS. [${computerScore}] COMPUTER`)
-    computerScore = 0, playerScore = 0;
-};
+
+
+let resetButton = document.getElementById('reset');
+const reset = () => {
+    let computerScore = 0, playerScore = 0;
+    let result = 'PLAY YOUR HAND';
+    let results = document.getElementById('results');
+    results.innerHTML = `<p>PLAYER [${playerScore}] <span>${result}</span> [${computerScore}] COMPUTER</p>`;
+}
+resetButton.addEventListener('click', reset);
+
+// let game = () => {
+//     let rounds = prompt('How many rounds of \'Rock, Paper, Scissors\' would you like to play?: ')
+//     computerScore = 0, playerScore = 0;
+//     for(i = 0; i < rounds; i++) {
+//         playRound();
+//     };
+//     alert(`Final Score: PLAYER [${playerScore}] VS. [${computerScore}] COMPUTER`)
+//     computerScore = 0, playerScore = 0;
+// };
